dfe0dfe6653c4c1b88b1313d8cb3d72e02494205
Jim Morris <morris@wolfman.com>
2013-06-05 02:32:04 -0700

    print M206 status

--- src/version.cpp
+++ src/version.cpp
@@ -1,6 +1,6 @@
 #include "version.h"
 const char *Version::get_build(void) const {
-    return "scissor-lift-hbot-55ef7a4";
+    return "scissor-lift-hbot-30f7cda";
 }
 const char *Version::get_build_date(void) const {
     return __DATE__ " " __TIME__;
