6ab0e4fee83940f9efd977c8b5ceb720fad8b2a3
Jim Morris <morris@wolfman.com>
2014-04-25 21:39:02 -0700

    remove plane class

--- src/modules/tools/zprobe/ZProbe.cpp
+++ src/modules/tools/zprobe/ZProbe.cpp
@@ -46,43 +46,6 @@
 #define Y_AXIS 1
 #define Z_AXIS 2
 
-#include "Vector3.h"
-// define a plane given three points
-class Plane
-{
-private:
-    Vector3 normal;
-    float d;
-
-public:
-    Plane(const Vector3 &v1, const Vector3 &v2, const Vector3 &v3)
-    {
-        // get the normal of the plane
-        Vector3 ab = v1.sub(v2);
-        Vector3 ac = v1.sub(v3);
-
-        Vector3 cp = ab.cross(ac);
-        normal = cp.unit();
-
-        // ax+by+cz+d=0
-        // solve for d
-        Vector3 dv = normal.mul(v1);
-        d = -dv[0] - dv[1] - dv[2];
-    }
-
-    // solve for z given x and y
-    // z= (-ax - by - d)/c
-    float getz(float x, float y)
-    {
-        return ((-normal[0] * x) - (normal[1] * y) - d) / normal[2];
-    }
-
-    Vector3 getNormal() const
-    {
-        return normal;
-    }
-};
-
 void ZProbe::on_module_loaded()
 {
     // if the module is disabled -> do nothing
